b873b31a877f599f64ad75437a08b75868975b24
Yahyaoui Nabil <yahyaouinabil1993@gmail.com>
2023-10-07 03:25:20 +0200

    initialize project

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-amber-600">
+      <h2 className="mt-48">Nabil</h2>
+    </div>
+  );
+}
+
+export default App;
