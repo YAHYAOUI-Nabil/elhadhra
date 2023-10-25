7128b95de23af22d4ca6b0494ef777f0e9696a46
Nabil Yahyaoui <yahyaouinabil1993@gmail.com>
2023-10-25 15:42:33 +0200

    create all landing page sections

--- client/src/pages/home/components/Tribunes.js
+++ client/src/pages/home/components/Tribunes.js
@@ -14,11 +14,11 @@ import 'swiper/css/navigation';
 // import required modules
 import { Autoplay, Pagination } from 'swiper/modules';
 
-const Chroniques = () => {
+const Tribunes = () => {
   return (
-    <div className='flex flex-col gap-2 my-4'>
-        <Title title="chroniques" color="black" />
-        <div>
+    <div className='flex flex-col gap-2 my-4 -ml-24 pl-24 py-3 pr-3 bg-[#F8EEE2]'>
+        <Title title="Tribunes" color="black" />
+        <div className='mt-4'>
             <Swiper
                 slidesPerView={3.5}
                 spaceBetween={30}
@@ -34,73 +34,66 @@ const Chroniques = () => {
             >
                 <SwiperSlide className="mb-10">
                     <div className='flex flex-col items-center justify-center gap-2 p-2'>
-                        <img className='w-40 h-40 rounded-full object-cover' src="https://livrenoir.fr/wp-content/uploads/2023/09/Thibault-de-Montbrial-1024x576.jpg" alt='Nabil' />
-                        <p className='text-center text-sm'>Nabil.Y: let's live in peaceful world without israel</p>
+                        <p className='text-center text-sm font-bold mb-2'>Nabil.Y: let's live in peaceful world without israel</p>
                         <div className='border-b-[2px] border-black w-16'></div>
-                        <p className='text-center text-sm'>Nabil Yahyaoui</p>
+                        <p className='text-center text-sm'>Nabil.Y: let's live in peaceful world without israel, Nabil.Y: let's live in peaceful world without israel</p>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide className="mb-10">
                     <div className='flex flex-col items-center justify-center gap-2 p-2'>
-                        <img className='w-40 h-40 rounded-full object-cover' src="https://livrenoir.fr/wp-content/uploads/2023/09/Erik-Tegner.jpg.webp" alt='Nabil' />
-                        <p className='text-center text-sm'>Nabil.Y: let's live in peaceful world without israel</p>
+                        <p className='text-center text-sm font-bold mb-2'>Nabil.Y: let's live in peaceful world without israel</p>
                         <div className='border-b-[2px] border-black w-16'></div>
-                        <p className='text-center text-sm'>Nabil Yahyaoui</p>
+                        <p className='text-center text-sm'>Nabil.Y: let's live in peaceful world without israel, Nabil.Y: let's live in peaceful world without israel</p>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide className="mb-10">
                     <div className='flex flex-col items-center justify-center gap-2 p-2'>
-                        <img className='w-40 h-40 rounded-full object-cover' src="https://livrenoir.fr/wp-content/uploads/2023/09/XD.jpg.webp" alt='Nabil' />
-                        <p className='text-center text-sm'>Nabil.Y: let's live in peaceful world without israel</p>
+                        <p className='text-center text-sm font-bold mb-2'>Nabil.Y: let's live in peaceful world without israel</p>
                         <div className='border-b-[2px] border-black w-16'></div>
-                        <p className='text-center text-sm'>Nabil Yahyaoui</p>
+                        <p className='text-center text-sm'>Nabil.Y: let's live in peaceful world without israel, Nabil.Y: let's live in peaceful world without israel</p>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide className="mb-10">
                     <div className='flex flex-col items-center justify-center gap-2 p-2'>
-                        <img className='w-40 h-40 rounded-full object-cover' src="https://livrenoir.fr/wp-content/uploads/2023/09/Jules-Laurens-1-1024x1024.jpg" alt='Nabil' />
-                        <p className='text-center text-sm'>Nabil.Y: let's live in peaceful world without israel</p>
+                        <p className='text-center text-sm font-bold mb-2'>Nabil.Y: let's live in peaceful world without israel</p>
                         <div className='border-b-[2px] border-black w-16'></div>
-                        <p className='text-center text-sm'>Nabil Yahyaoui</p>
+                        <p className='text-center text-sm'>Nabil.Y: let's live in peaceful world without israel, Nabil.Y: let's live in peaceful world without israel</p>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide className="mb-10">
                     <div className='flex flex-col items-center justify-center gap-2 p-2'>
-                        <img className='w-40 h-40 rounded-full object-cover' src="https://livrenoir.fr/wp-content/uploads/2023/09/Thibault-de-Montbrial-1024x576.jpg" alt='Nabil' />
-                        <p className='text-center text-sm'>Nabil.Y: let's live in peaceful world without israel</p>
+                        <p className='text-center text-sm font-bold mb-2'>Nabil.Y: let's live in peaceful world without israel</p>
                         <div className='border-b-[2px] border-black w-16'></div>
-                        <p className='text-center text-sm'>Nabil Yahyaoui</p>
+                        <p className='text-center text-sm'>Nabil.Y: let's live in peaceful world without israel, Nabil.Y: let's live in peaceful world without israel</p>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide className="mb-10">
                     <div className='flex flex-col items-center justify-center gap-2 p-2'>
-                        <img className='w-40 h-40 rounded-full object-cover' src="https://livrenoir.fr/wp-content/uploads/2023/09/Erik-Tegner.jpg.webp" alt='Nabil' />
-                        <p className='text-center text-sm'>Nabil.Y: let's live in peaceful world without israel</p>
+                        <p className='text-center text-sm font-bold mb-2'>Nabil.Y: let's live in peaceful world without israel</p>
                         <div className='border-b-[2px] border-black w-16'></div>
-                        <p className='text-center text-sm'>Nabil Yahyaoui</p>
+                        <p className='text-center text-sm'>Nabil.Y: let's live in peaceful world without israel, Nabil.Y: let's live in peaceful world without israel</p>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide className="mb-10">
                     <div className='flex flex-col items-center justify-center gap-2 p-2'>
-                        <img className='w-40 h-40 rounded-full object-cover' src="https://livrenoir.fr/wp-content/uploads/2023/09/XD.jpg.webp" alt='Nabil' />
-                        <p className='text-center text-sm'>Nabil.Y: let's live in peaceful world without israel</p>
+                        <p className='text-center text-sm font-bold mb-2'>Nabil.Y: let's live in peaceful world without israel</p>
                         <div className='border-b-[2px] border-black w-16'></div>
-                        <p className='text-center text-sm'>Nabil Yahyaoui</p>
+                        <p className='text-center text-sm'>Nabil.Y: let's live in peaceful world without israel, Nabil.Y: let's live in peaceful world without israel</p>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide className="mb-10">
                     <div className='flex flex-col items-center justify-center gap-2 p-2'>
-                        <img className='w-40 h-40 rounded-full object-cover' src="https://livrenoir.fr/wp-content/uploads/2023/09/Jules-Laurens-1-1024x1024.jpg" alt='Nabil' />
-                        <p className='text-center text-sm'>Nabil.Y: let's live in peaceful world without israel</p>
+                        <p className='text-center text-sm font-bold mb-2'>Nabil.Y: let's live in peaceful world without israel</p>
                         <div className='border-b-[2px] border-black w-16'></div>
-                        <p className='text-center text-sm'>Nabil Yahyaoui</p>
+                        <p className='text-center text-sm'>Nabil.Y: let's live in peaceful world without israel, Nabil.Y: let's live in peaceful world without israel</p>
                     </div>
                 </SwiperSlide>
+                
             </Swiper>
         </div>
         <div className='flex justify-end -mt-2'>
             <div className='flex flex-row gap-2 items-center'>
-            <NavLink to={'/'} className='uppercase font-semibold text-xl'>toutes les chroniques</NavLink>
+            <NavLink to={'/'} className='uppercase font-semibold text-xl'>toutes les tribunes</NavLink>
             <NavLink to={'/'} className='flex items-center'><MdOutlineKeyboardArrowRight className='h-6 w-6' /></NavLink>
             </div>
         </div>
@@ -108,4 +101,4 @@ const Chroniques = () => {
   )
 }
 
-export default Chroniques
+export default Tribunes
