2e428c2788a3e53e87be554c8a16001d4670aa79
Nabil Yahyaoui <yahyaouinabil1993@gmail.com>
2023-11-04 18:47:21 +0100

    add scroll to top feat

--- client/src/App.js
+++ client/src/App.js
@@ -11,10 +11,12 @@ import ArticlesByCategory from "./pages/articlesByCategory";
 import ArticleDetails from "./pages/articlesDetails";
 import AboutUS from "./pages/aboutUs";
 import SideBar from "./components/SideBar";
+import ScrollToTop from "./utils/ScrollToTop";
+
 import { useStateContext } from "./context/ContextProvider";
 
 function App() {
-  const { activeMenu, setActiveMenu } = useStateContext();
+  const { activeMenu } = useStateContext();
   return (
     <BrowserRouter>
       <div className="relative">
@@ -27,13 +29,15 @@ function App() {
         }
         <div>
           <Header />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/qui-sommes-nous" element={<AboutUS />} />
-            <Route path="/mon-compte" element={<Account />} />
-            <Route path="/category/:category" element={<ArticlesByCategory />} />
-            <Route path="/category/article/:id" element={<ArticleDetails />} />
-          </Routes>
+          <ScrollToTop>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/qui-sommes-nous" element={<AboutUS />} />
+              <Route path="/mon-compte" element={<Account />} />
+              <Route path="/category/:category" element={<ArticlesByCategory />} />
+              <Route path="/category/article/:id" element={<ArticleDetails />} />
+            </Routes>
+          </ScrollToTop>
           <Footer />
         </div>
       </div>
